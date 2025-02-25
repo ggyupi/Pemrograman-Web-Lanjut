a76950b748a78febd1c75f4fd36a9e451470787a
ggyupi <beryl32451@gmail.com>
2025-02-25 16:50:17 +0700

    Route Parameters dan Optional Parameters

--- week2/PWL/routes/web.php
+++ week2/PWL/routes/web.php
@@ -30,3 +30,19 @@ Route::get('/selamat', function () {
 Route::get('/about', function () { 
     return 'NIM : 2341720256, Nama : Beryl Funky Mubarok'; 
     }); 
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+    }); 
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+    }); 
+
+Route::get('/articles/{id}', function ($id1) { 
+    return 'Halaman Artikel dengan ID '.$id1; 
+    }); 
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name; 
+}); 
