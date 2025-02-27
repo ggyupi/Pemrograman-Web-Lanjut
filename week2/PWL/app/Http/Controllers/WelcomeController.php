ff1cdc98c8ab111a4ee312c1d7e04b05200aa761
ggyupi <beryl32451@gmail.com>
2025-02-27 15:01:11 +0700

    Meneruskan data ke view

--- week2/PWL/app/Http/Controllers/WelcomeController.php
+++ week2/PWL/app/Http/Controllers/WelcomeController.php
@@ -10,6 +10,8 @@ class WelcomeController extends Controller
         return 'Hello World'; 
     } 
     public function greeting(){ 
-        return view('blog.hello', ['name' => 'Beryl Funky Mubarok']); 
+        return view('blog.hello')
+            ->with('name','Beryl Funky Mubarok') 
+            ->with('occupation','Pemancing Handal'); 
     } 
 }
