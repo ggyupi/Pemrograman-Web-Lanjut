b659165f15d13388279d9609222e16da57a2b9af
ggyupi <beryl32451@gmail.com>
2025-02-27 12:17:03 +0700

    Membuat Controller

--- week2/PWL/routes/web.php
+++ week2/PWL/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
+// use App\Http\Controllers\PhotoController; 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,13 +21,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class,"index"]);
 
-Route::get('/hello', function () { 
-    return 'Hello World'; 
-    }); 
+Route::get('/hello',  [WelcomeController::class,"hello"]);
 
 Route::get('/world', function () { 
     return 'World'; 
@@ -27,9 +31,7 @@ Route::get('/world', function () {
 Route::get('/selamat', function () { 
     return 'Selamat Datang'; 
     }); 
-Route::get('/about', function () { 
-    return 'NIM : 2341720256, Nama : Beryl Funky Mubarok'; 
-    }); 
+Route::get('/about', [AboutController::class,"about"]); 
 
 Route::get('/user/{name}', function ($name) { 
     return 'Nama saya '.$name; 
@@ -39,10 +41,9 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
     }); 
 
-Route::get('/articles/{id}', function ($id1) { 
-    return 'Halaman Artikel dengan ID '.$id1; 
-    }); 
+Route::get('/articles/{id}', [ArticleController::class,"articles"]); 
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name; 
 }); 
+// Route::resource('photos', PhotoController::class); 
