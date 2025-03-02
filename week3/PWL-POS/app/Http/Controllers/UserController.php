803d7835b6668d3eabaff0e37bb8270e73b129e8
ggyupi <beryl32451@gmail.com>
2025-03-02 21:31:43 +0700

    Week3 : Praktikum 6 - ELOQUENT ORM , implementasi Eloquent ORM

--- week3/PWL-POS/app/Http/Controllers/UserController.php
+++ week3/PWL-POS/app/Http/Controllers/UserController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $data = [
+            'username' =>'Pelanggan 1'
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+        $data = UserModel::all(); //mengambil semua data dari tabel m_user
+        return view('user', ['data' => $data]); 
+    }
 }
