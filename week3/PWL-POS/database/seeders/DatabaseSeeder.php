5890085e5bec9fca3212daa088dc47c54a1cb8e8
ggyupi <beryl32451@gmail.com>
2025-03-02 17:23:26 +0700

    Week3 : Praktikum 3 - Seeder, Membuat file seeder

--- week3/PWL-POS/database/seeders/DatabaseSeeder.php
+++ week3/PWL-POS/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            LevelSeeder::class,
+            KategoriSeeder::class,
+            BarangSeeder::class,
+            UserSeeder::class,
+            StockSeeder::class,
+            PenjualanSeeder::class,
+            PenjualanDetailSeeder::class,
+        ]);
     }
 }
