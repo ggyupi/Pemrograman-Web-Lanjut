6da1d3d7338cc98dc2550fd6703037b2d1da9ee1
ggyupi <beryl32451@gmail.com>
2025-03-09 15:43:25 +0700

    Week4 : Praktikum 2.2 – Not Found Exceptions

--- PWL-POS/app/Http/Controllers/UserController.php
+++ PWL-POS/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Illuminate\database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
@@ -35,9 +36,13 @@ class UserController extends Controller
         
         //JS 4- praktikum 2.1 – Retrieving Single Models
         // $user = UserModel::firstWhere('level_id', 1);
-        $user = UserModel::findOrFail(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOrFail(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        //JS 4- praktikum 2.2 - Not Found Exceptions
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
